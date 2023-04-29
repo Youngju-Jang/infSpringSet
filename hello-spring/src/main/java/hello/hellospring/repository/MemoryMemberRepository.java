ef2253be91121ba0dadfcb39a7a66f02bf560b93
youngju-jang <joj1043@nate.com>
2023-04-29 16:18:12 +0900

    [Feat] 회원 서비스 개발, 테스트
    - repository di 적용
    
    #6

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -28,6 +28,7 @@ public class MemoryMemberRepository implements MemberRepository{
                .findAny();
      }
      
+     
      @Override
      public List<Member> findAll() {
           return new ArrayList<>(store.values());
