7880ef7f3cf34c6cf64ab155c849581c9e69062e
youngju-jang <joj1043@nate.com>
2023-04-29 17:14:23 +0900

    [FEAT] 4.2. 자바 코드로 직접 스프링 빈 등록하기
    Related to #6

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -10,10 +10,18 @@ public class MemberController {
      // 이렇게 할 경우, 다른 컨트롤러에서 서비스를 쓸 때, 여러 인스턴스를 생성하게 됨
 //     private final MemberService memberService = new MemberService();
      
-     private final MemberService membrService;
+     // 1.필드주입방식 : 바꿀 방법이 없음. 비추
+//     @Autowired private MemberService memberService;
      
+     // 2. setter 주입 : public으로 열려있어야 함. 노출이 되어버려. 잘못바뀌면 문제됨(보통 한번셋팅후 볼일없음)
+     private MemberService memberService;
+//     @Autowired
+//     public void setMemberService(MemberService memberService){
+//          this.memberService = memberService;
+//     }
+     // 3. 생성자 주입방식 : 생성자 주입시점에 넣고 변경을 막아줌
      @Autowired
-     public MemberController(MemberService membrService) { //DI . bean컨테이너가 관리하도록. //Component scan방식
-          this.membrService = membrService;
+     public MemberController(MemberService memberService) { //DI . bean컨테이너가 관리하도록. //Component scan방식
+          this.memberService = memberService;
      }
 }
